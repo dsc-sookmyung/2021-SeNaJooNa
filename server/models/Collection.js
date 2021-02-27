a8080e2f27318560c7e77122a7b4dfcd858af883
NakyongLee <lnk7424@sookmyung.ac.kr>
2021-02-28 00:26:07 +0900

    add collection crud base

--- server/models/Collection.js
+++ server/models/Collection.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const collectionSchema = Schema({
+    //front쪽 local에 저장되어 있음
     creator: {
         type: Schema.Types.ObjectId,
         ref: 'User'
@@ -9,6 +10,7 @@ const collectionSchema = Schema({
     title: {
         type: String,
     },
+    //나중에 따로 처리 경로만 저장
     thumbnail: {
         type: String
     },
---
+++ server/routes/collections.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const { Collection } = require('../models/Collection');
+
+router.post("/", (req, res) => {
+    const collection = new Collection(req.body)
+
+    collection.save((err, collection) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
+
+router.get("/", (req, res) => {
+    Collection.find({}, function (err, Collection) {
+        if (err) return res.status(500).send("Collection failed");
+        res.status(200).send({ success: true, Collection })
+    })
+});
+
+router.get("/:id", (req, res) => {
+    Collection.findOne({ _id: req.params.id })
+        .exec((err, collection) => {
+            if (err) return res.status(500).send("Category Collection failed");
+            if (!collection) return res.status(404).send("No Collection");
+            res.status(200).send({ success: true, collection });
+        });
+});
+
+router.put("/:id", (req, res) => {
+    Collection.findByIdAndUpdate({ _id: req.params.id }, req.body, { new: true })
+        .exec((err, collection) => {
+            if (err) return res.status(500).send("Collection Update failed");
+            res.status(200).send({ success: true, collection });;
+        })
+})
+
+router.delete("/:id", (req, res) => {
+    Collection.findByIdAndDelete({ _id: req.params.id })
+        .exec((err, collection) => {
+            if (err) return res.status(500).send("Collection Delete failed");
+            res.status(200).send({ success: true });
+        })
+})
+
+module.exports = router;
