39a1967b5dabb64b271b2c46cfbf493a94ce42e7
NakyongLee <lnk7424@sookmyung.ac.kr>
2021-02-27 17:35:56 +0900

    fix categorySchema

--- server/models/Category.js
+++ server/models/Category.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const categorySchema = Schema({
-    categoryID: {
-        type: String,
-    },
     title: {
         type: String,
     },
-
+    description: {
+        type: String,
+    },
     thumbnail: {
         type: String
     }
