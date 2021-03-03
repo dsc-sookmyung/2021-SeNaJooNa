7a221c71aaa8dde10e7be3cdcea549e187f966ed
SeryoungShim <csssr1207@gmail.com>
2021-03-03 21:57:02 +0900

    placeId schema string->Objectid & like_count update(POST/DELETE)/default=0

--- server/routes/likeplaces.js
+++ server/routes/likeplaces.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 
 const { PlaceLike } = require("../models/PlaceLike");
+const { Place } = require('../models/Place');
 const { auth } = require("../middleware/auth");
+
+router.get('/', auth, (req, res) => {
+    // PlaceLike.find({ userId: req.user._id }, function(err, doc) {
+    //     if (err) return res.json({ success: false, err });
+    //     return res.status(200).json(doc);
+    // })
+    PlaceLike.find({ userId: req.user._id }).populate('placeId').exec( (err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).send(doc);
+    })
+})
 
 router.get('/:placeId', auth, (req, res) => {
     PlaceLike.findOne({ userId:req.user._id, placeId:req.params.placeId }, function(err, doc) {
         if (err) return res.json({ success: false, err });
-        return res.status(200).json({ success: true, like: true });
-    })
-})
-
-router.get('/getPlaces', auth, (req, res) => {
-    PlaceLike.find({ userId:req.user._id }, function(err, doc) {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json(doc);
+        if(doc)
+            return res.status(200).json({ success: true, like: true });
+        return res.status(200).json({ success:true, like: false})
     })
 })
 
@@ -33,9 +41,14 @@ router.post("/", auth, (req, res) => {
 
     placeLike.save((err, doc) => {
         if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
-        });
+        PlaceLike.countDocuments({placeId: req.body.placeId}).then((count)=>{
+            Place.findOneAndUpdate({_id: req.body.placeId}, {$set : {like_count: count}}, (err, doc)=>{
+                console.log(err)
+                return res.status(200).json({
+                    success: true
+                });
+            })
+        })
     });
 });
 
@@ -43,8 +56,15 @@ router.delete("/:placeId", auth, (req, res) => {
 
     PlaceLike.deleteOne({ placeId:req.params.placeId, userId:req.user._id }, function (err) {
         if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
-        });
+        PlaceLike.countDocuments({placeId: req.body.placeId}).then((count)=>{
+            Place.findOneAndUpdate({_id: req.body.placeId}, {$set : {like_count: count}}, (err, doc)=>{
+                console.log(err)
+                return res.status(200).json({
+                    success: true
+                });
+            })
+        })
     })
 });
+
+module.exports = router;
