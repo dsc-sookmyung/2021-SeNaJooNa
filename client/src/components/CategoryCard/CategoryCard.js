491a55bfa2832561879d55a33e9c3afc40c1b9ea
SeryoungShim <csssr1207@gmail.com>
2021-03-13 21:49:40 +0900

    add collection divs from db

--- client/src/components/CategoryCard/CategoryCard.js
+++ client/src/components/CategoryCard/CategoryCard.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import styles from './CategoryCard.module.css';
 
 function CategoryCard(props) {
+    const [recommend, setRecommend] = useState([])
+    useEffect(() => {
+        const findCategory = (element) => element._id === props.category._id
+        const idx = props.categories.findIndex(findCategory)
+        const category_length = props.categories.length
+        if(category_length > 2){
+            setRecommend([props.categories[idx-1<0?category_length-1:idx-1], props.categories[idx+1>=category_length?0:idx+1]])
+        } else if(props.categories.length > 1){
+            setRecommend([props.categories[idx+1>=category_length?0:idx+1]])
+        }
+        
+    }, [])
     return (
         <div className={styles.categoryCard}>
             <Link to={{
                 pathname: '/second',
                 state: {
-                    categoryId: props.category._id
+                    categoryId: props.category._id,
+                    recommend: recommend
                 }
             }}>
                 <img className={styles.img} src={props.category.thumbnail} alt={props.category.title} />
--- client/src/pages/SecondPage/SecondPage.js
+++ client/src/pages/SecondPage/SecondPage.js
@@ -1,46 +1,59 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 import styles from './SecondPage.module.css';
 
 import CollectionCard from '../../components/CollectionCard/CollectionCard';
 
-function SecondPage() {
+function SecondPage(props) {
+    const [collections, setCollections] = useState([])
+    const [recommend, setRecommend] = useState([])
+
+    function getRecommend(rec){
+        return new Promise(function(resolve, reject){
+            axios.get(`/api/collections/category?categoryId=${rec._id}`).then((response)=>{
+                resolve({ category:rec.description, collection: response.data.collection.slice(0,6)}) 
+            })
+        })
+    }
+    
+    useEffect(() => {
+        axios.get(`/api/collections/category?categoryId=${props.location.state.categoryId}`).then((response)=>{
+            setCollections(response.data.collection)
+        })
+        Promise.all(props.location.state.recommend.map(getRecommend)).then((result)=>{
+            setRecommend(result);
+        })
+    }, []);
     return (
         <div>
             <div className={styles.part1}>
                 <div className={styles.text}>당신이 선택한 카테고리에는 이런 컬렉션들이 존재합니다. </div>
                 <div className={`${styles.gridContainer} ${styles.grid4x2}`}>
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
+                    {collections.map((collection)=>(
+                        <CollectionCard collection={collection} key={collection._id} />
+                    ))}
                 </div>
                 <div className={styles.previousNext}>
                     <a href='#' className={styles.button}>&#8249;</a>
                     <a href='#' className={styles.button}>&#8250;</a>
                 </div>
             </div>
-            <div className={styles.part2}>
-                <div className={styles.text}>다른 카테고리가 궁금하신가요?</div>
-                <div className={styles.text}>이런 카테고리도 존재합니다. </div>
-                <div className={`${styles.gridContainer} ${styles.grid4x1}`}>
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                </div>
-                <div className={styles.text}>이런 카테고리도 존재합니다. </div>
-                <div className={`${styles.gridContainer} ${styles.grid4x1}`}>
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                    <CollectionCard />
-                </div>
-            </div>
+            {
+                recommend.map((rec)=>(
+                    // rec.collection.length > 0 &&
+                    <div className={styles.part2}>
+                        <div className={styles.text}>다른 카테고리가 궁금하신가요?</div>
+                        <div className={styles.text}>{rec.category}</div>
+                        <div className={`${styles.gridContainer} ${styles.grid4x1}`}>
+                            {rec.collection.map((collection)=>(
+                                <CollectionCard collection={collection} key={collection._id} />
+                            ))}
+                        </div>
+                    </div>
+                ))
+            }
+            
         </div>
     )
 }
