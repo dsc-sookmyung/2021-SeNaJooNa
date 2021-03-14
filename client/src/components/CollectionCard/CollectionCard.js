ccd48a7281c39f8b452314d4c635635d0e108ab7
SeryoungShim <csssr1207@gmail.com>
2021-03-14 14:10:23 +0900

    add CollectionCard Link second -> third

--- client/src/components/CollectionCard/CollectionCard.js
+++ client/src/components/CollectionCard/CollectionCard.js
@@ -1,9 +1,15 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import styles from './CollectionCard.module.css';
 
 function CollectionCard(props) {
     return (
+        <Link to={{
+            pathname: '/third',
+            state: {
+                collection: props.collection
+            }
+        }}>
         <div className={styles.collectionCard}>
             <img className={styles.img} src={props.collection.thumbnail}/>
             <div className={styles.content}>
@@ -12,7 +18,9 @@ function CollectionCard(props) {
                 <button className={styles.like}>🤍좋아요 {props.collection.like}</button>
             </div>
             {/* <div className="collection-img-cover"></div> */}
+            
         </div>
+        </Link>
     )
 }
 
