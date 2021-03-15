3fbcb9d1f4ea472f957baf8538f2a8e6b37461a8
parang99 <silsoo99@naver.com>
2021-03-15 15:42:27 +0900

    edit PlaceCard

--- client/src/components/PlaceCard/PlaceCard.js
+++ client/src/components/PlaceCard/PlaceCard.js
@@ -5,8 +5,8 @@ import styles from './PlaceCard.module.css';
 function PlaceCard() {
     return (
         <div className={styles.placeCard}>
-            <div className={styles.like}>🤍좋아요</div>
-            <div>
+            <span className={styles.like}>🤍 000</span>
+            <div className={styles.imgContainer}>
                 <img className={styles.img} src="https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTAyMDVfMTky%2FMDAxNjEyNDg3MDQxMDcy.i8O1gb8ZJVuDDl_XEB4Yn_XOj6lPUWuwNPOkhoeRNqkg.2tYCmC3zkx0Qfn4YNQ1pKhtnqcfbQzz_nNXMrbNYyuwg.JPEG.rlawnstjs43%2FIMG_2463.jpg&type=sc960_832" />
             </div>
             <div className={styles.title}>장소 이름</div>
