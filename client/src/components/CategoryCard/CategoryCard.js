757208f32e4cd86dd0b593c63e1e84fca367fcb0
parang99 <silsoo99@naver.com>
2021-03-15 15:07:31 +0900

    edit css in LoginRegisterPage, modal in MyPage and  CategoryCard. also change between <Link> and <div> in CategoryCard.

--- client/src/components/CategoryCard/CategoryCard.js
+++ client/src/components/CategoryCard/CategoryCard.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './CategoryCard.module.css';
 
@@ -8,30 +8,32 @@ function CategoryCard(props) {
         const findCategory = (element) => element._id === props.category._id
         const idx = props.categories.findIndex(findCategory)
         const category_length = props.categories.length
-        if(category_length > 2){
-            setRecommend([props.categories[idx-1<0?category_length-1:idx-1], props.categories[idx+1>=category_length?0:idx+1]])
-        } else if(props.categories.length > 1){
-            setRecommend([props.categories[idx+1>=category_length?0:idx+1]])
+        if (category_length > 2) {
+            setRecommend([props.categories[idx - 1 < 0 ? category_length - 1 : idx - 1], props.categories[idx + 1 >= category_length ? 0 : idx + 1]])
+        } else if (props.categories.length > 1) {
+            setRecommend([props.categories[idx + 1 >= category_length ? 0 : idx + 1]])
         }
-        
+
     }, [])
     return (
-        <div className={styles.categoryCard}>
-            <Link to={{
-                pathname: '/second',
-                state: {
-                    categoryId: props.category._id,
-                    recommend: recommend
-                }
-            }}>
+
+        <Link to={{
+            pathname: '/second',
+            state: {
+                categoryId: props.category._id,
+                recommend: recommend
+            }
+        }}>
+            <div className={styles.categoryCard}>
                 <img className={styles.img} src={props.category.thumbnail} alt={props.category.title} />
                 <div className={styles.content}>
                     <div className={styles.title}>{props.category.title}</div>
                     <div className={styles.detail}>{props.category.description}</div>
                 </div>
-                <div className={styles.imgCover}></div>
-            </Link>
-        </div>
+                {/* <div className={styles.imgCover}></div> */}
+            </div>
+        </Link>
+
     )
 }
 
