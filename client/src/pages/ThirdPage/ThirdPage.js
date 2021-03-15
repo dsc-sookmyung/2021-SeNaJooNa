80ddb0aeb2ef17467b3ac818b3f56e75276b9f5c
parang99 <silsoo99@naver.com>
2021-03-15 16:15:55 +0900

    delete map part in ThirdPage

--- client/src/pages/ThirdPage/ThirdPage.js
+++ client/src/pages/ThirdPage/ThirdPage.js
@@ -7,31 +7,31 @@ import PlaceCard from '../../components/PlaceCard/PlaceCard';
 function ThirdPage() {
     return (
         <div className={styles.thirdPage}>
-            <div className={styles.left}>
-                <div>
-                    <div className={styles.text}>
-                        특정 카테고리명 &#8250; 특정 컬렉션명
+            {/* <div className={styles.left}> */}
+            <div>
+                <div className={styles.text}>
+                    특정 카테고리명 &#8250; 특정 컬렉션명
                         </div>
-                    <button className={styles.like}>❤ Collection Like</button>
-                </div>
-                <div className={styles.gridContainer}>
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
-                    <PlaceCard />
+                <button className={styles.like}>❤ Collection Like</button>
+            </div>
+            <div className={styles.gridContainer}>
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
+                <PlaceCard />
 
-                </div>
             </div>
-            <div className={styles.right}>
+            {/* </div> */}
+            {/* <div className={styles.right}>
                 지도
-            </div>
+            </div> */}
         </div>
     )
 }
