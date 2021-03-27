02a139fa54b48e1c8744f394ffb16b8e5cca84d7
parang99 <silsoo99@naver.com>
2021-03-27 15:29:23 +0900

    add text-decoration none in Link tag in PlaceCard to remove blue underline in title and detail

--- client/src/components/PlaceCard/PlaceCard.js
+++ client/src/components/PlaceCard/PlaceCard.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, withRouter } from 'react-router-dom';
 import styles from './PlaceCard.module.css';
 import { useDispatch } from 'react-redux'
@@ -14,7 +14,7 @@ function PlaceCard(props) {
 
     const dispatch = useDispatch();
     useEffect(() => {
-        if(props.place.thumbnail.length === 0){
+        if (props.place.thumbnail.length === 0) {
             setImage("https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAyMTAyMDVfMTky%2FMDAxNjEyNDg3MDQxMDcy.i8O1gb8ZJVuDDl_XEB4Yn_XOj6lPUWuwNPOkhoeRNqkg.2tYCmC3zkx0Qfn4YNQ1pKhtnqcfbQzz_nNXMrbNYyuwg.JPEG.rlawnstjs43%2FIMG_2463.jpg&type=sc960_832")
         } else {
             setImage(props.place.thumbnail[0])
@@ -22,8 +22,8 @@ function PlaceCard(props) {
         dispatch(auth()).then(response => {
             if (response.payload.isAuth) {
                 setIsAuth(true)
-                axios.get(`/api/like/place/${props.place._id}`).then((response)=>{
-                    if(response.data.like)
+                axios.get(`/api/like/place/${props.place._id}`).then((response) => {
+                    if (response.data.like)
                         setIsLiked(true)
                     else
                         setIsLiked(false)
@@ -31,40 +31,40 @@ function PlaceCard(props) {
             }
         })
     }, [])
-    function OnLikeHandler(){
-        if(isAuth){
-            if(!isLiked){
-                axios.post(`/api/like/place`,{placeId:props.place._id}).then((response)=>{
-                    setLike(like+1)
+    function OnLikeHandler() {
+        if (isAuth) {
+            if (!isLiked) {
+                axios.post(`/api/like/place`, { placeId: props.place._id }).then((response) => {
+                    setLike(like + 1)
                     setIsLiked(true)
                 })
             } else {
-                axios.delete(`/api/like/place/${props.place._id}`).then((response)=>{
-                    setLike(like-1)
+                axios.delete(`/api/like/place/${props.place._id}`).then((response) => {
+                    setLike(like - 1)
                     setIsLiked(false)
                 })
             }
         } else {
-            window.location.href="/login"
+            window.location.href = "/login"
         }
     }
     return (
-            <div className={styles.placeCard}>
-                <span className={styles.like} onClick={OnLikeHandler}>{isLiked?"❤️":"🤍"} {like}</span>
-                {/* <Link to={{
+        <div className={styles.placeCard}>
+            <span className={styles.like} onClick={OnLikeHandler}>{isLiked ? "❤️" : "🤍"} {like}</span>
+            {/* <Link to={{
                     pathname: `/fourth?collection=${props.collection}&place=${props.place._id}`,
                     state: {
                         place: props.place
                     }
                 }}> */}
-                <Link to={`/place?collection=${props.collection}&place=${props.place._id}`}>
-                    <div className={styles.imgContainer}>
-                        <img className={styles.img} src={image} />
-                    </div>
-                    <div className={styles.title}>{props.place.name}</div>
-                    <div className={styles.detail}>{props.place.description}</div>
-                </Link>
-            </div>
+            <Link to={`/place?collection=${props.collection}&place=${props.place._id}`} className={styles.linkTag}>
+                <div className={styles.imgContainer}>
+                    <img className={styles.img} src={image} />
+                </div>
+                <div className={styles.title}>{props.place.name}</div>
+                <div className={styles.detail}>{props.place.description}</div>
+            </Link>
+        </div>
     )
 }
 
