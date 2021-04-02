6d95ed81e46e0e282c7a2465b559b647e72d1acd
NakyongLee <lnk7424@sookmyung.ac.kr>
2021-04-02 20:22:17 +0900

    update collection like

--- client/src/pages/CollectionPage/CollectionPage.js
+++ client/src/pages/CollectionPage/CollectionPage.js
@@ -5,17 +5,15 @@ import styles from './ThirdPage.module.css';
 
 import PlaceCardsDiv from '../../components/PlaceCard/PlaceCardsDiv'
 import { withRouter } from 'react-router';
-import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux'
 import { auth } from '../../actions/user_action'
-import Favorite from './Favorite';
 
 function CollectionPage(props) {
     const [places, setPlaces] = useState([])
     const [collection, setCollection] = useState({});
     const [isAuth, setIsAuth] = useState(false)
     const [user, setUser] = useState("")
-
+    const [isLiked, setIsLiked] = useState(false)
     const dispatch = useDispatch();
 
     useEffect(() => {
@@ -23,11 +21,14 @@ function CollectionPage(props) {
             if (response.payload.isAuth) {
                 setIsAuth(true)
                 setUser(response.payload._id)
+                axios.get(`/api/likeCollections/${props.location.state.collection._id}`).then((response) => {
+                    if (response.data.like)
+                        setIsLiked(true)
+                    else
+                        setIsLiked(false)
+                })
             }
         })
-    }, [])
-
-    useEffect(() => {
         axios.get(`/api/places/${props.location.state.collection._id}`).then((response) => {
             setPlaces(response.data)
         })
@@ -35,7 +36,11 @@ function CollectionPage(props) {
         axios.get(`/api/collections/${props.location.state.collection._id}`).then((response) => {
             setCollection(response.data.collection)
         })
-    }, []);
+    }, [])
+
+    // useEffect(() => {
+
+    // }, []);
 
     const onDeleteHandler = () => {
         if (places.length !== 0) {
@@ -54,6 +59,25 @@ function CollectionPage(props) {
         props.history.push(`makeCollection/${props.location.state.collection._id}`)
     }
 
+    const collectionLikeHandler = () => {
+        const doc = {
+            "collectionId": collection._id
+        }
+        console.log(doc);
+        if (isLiked) {
+            axios.delete('/api/likeCollections', { data: doc }).then((response) => {
+                setIsLiked(false);
+            })
+        }
+        else {
+            console.log("좋아요")
+            axios.post('/api/likeCollections', doc).then((response) => {
+                setIsLiked(true);
+            })
+        }
+    }
+
+
     return (
         <div className={styles.collectionPage}>
             <div>
@@ -63,13 +87,16 @@ function CollectionPage(props) {
                 <div className={styles.textSmall}>
                     {collection.content}
                 </div>
-                {(isAuth && user !== collection.creator) ?
-                    <Favorite collection={collection} collectionId={collection._id} userId={user} /> :
+                {(isAuth && user === collection.creator) ?
                     <div>
                         <button onClick={onDeleteHandler} className={styles.like}>삭제</button>
                         <button onClick={onUpdateHandler} className={styles.like}>수정</button>
-                    </div>
+                    </div> : null
+
                 }
+                <div>
+                    <button onClick={collectionLikeHandler} className={styles.like}>{isLiked ? "❤" : "♡"} Collection Like </button>
+                </div>
             </div>
             <PlaceCardsDiv isAuth={isAuth} user={user} collection={props.location.state.collection} />
         </div>
